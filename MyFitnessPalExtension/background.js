f495c9804f74d0d82f46df0d43e085927a3fa5ed
John-Gee <john.ettedgui@gmail.com>
2012-07-19 19:15:10 -0700

    first git release

---
+++ MyFitnessPalExtension/background.js
@@ -0,0 +1,23 @@
+//Called when the url of a tab changes.
+function checkForValidUrl(tabId, changeInfo, tab) {
+	// if the url contains myfitnesspal
+	var myFitnessPalString = "myfitnesspal";
+	if(tab.url.indexOf(myFitnessPalString) != -1)
+	{
+		// show the page action
+		chrome.pageAction.show(tabId);
+	}
+};
+
+        
+chrome.extension.onMessage.addListener(function(msg, _, sendResponse) {
+  
+      sendResponse(JSON.stringify([
+                localStorage["days"],
+                localStorage["data"]
+                  ]));
+  
+  
+  // If we don't return anything, the message channel will close, regardless
+  // of whether we called sendResponse.
+});
