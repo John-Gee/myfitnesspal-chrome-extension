f25e62771d5b25d50f5add760de37e84538b3613
John-Gee <john.ettedgui@gmail.com>
2013-09-05 23:55:02 -0700

    Remove the ugly dashed bar Remove some google ads Fixed the color of the remaining amounts

--- MyFitnessPalExtension/content.js
+++ MyFitnessPalExtension/content.js
@@ -101,7 +101,10 @@ function AddDerivedCalories(mealHeaderElement, indexes)
             {
                 var totalCalories = CalculateTotalCaloriesFromStrMacros(tds[indexes.Carbs].innerHTML, tds[indexes.Fat].innerHTML, tds[indexes.Protein].innerHTML);          
                 if(!isNaN(totalCalories))
-                    InsertClonedNodeAfter(trs[i], tds[j], totalCalories);
+                {
+                    var newNode = InsertClonedNodeAfter(trs[i], tds[j], totalCalories);
+                    SetColorElement(newNode);
+                }
                 break;
             }
         }
@@ -151,11 +154,43 @@ function ReplaceGoals(totalCalories, totalCarbs, totalFat, totalProtein)
 	var remainingFatId = remainingCarbId + 1;
 	var remainingProteinId = remainingFatId + 1;
 	
-	elems[remainingCaloriesId].innerHTML = parseInt(totalCalories)  - parseInt(elems[currentCaloriesId].innerHTML.replace(",", ""));
+	/*elems[remainingCaloriesId].innerHTML = parseInt(totalCalories)  - parseInt(elems[currentCaloriesId].innerHTML.replace(",", ""));        
 	elems[remainingCarbId].innerHTML = parseInt(totalCarbs) - parseInt(elems[currentCarbId].innerHTML.replace(",", ""));
 	elems[remainingFatId].innerHTML = parseInt(totalFat) - parseInt(elems[currentFatId].innerHTML.replace(",", ""));
-	elems[remainingProteinId].innerHTML = parseInt(totalProtein) - parseInt(elems[currentProteinId].innerHTML.replace(",", ""));
+	elems[remainingProteinId].innerHTML = parseInt(totalProtein) - parseInt(elems[currentProteinId].innerHTML.replace(",", ""));*/
+        
+        UpdateRemainingElement(elems[remainingCaloriesId], totalCalories, elems[currentCaloriesId].innerHTML);
+        UpdateRemainingElement(elems[remainingCarbId], totalCarbs, elems[currentCarbId].innerHTML);
+        UpdateRemainingElement(elems[remainingFatId], totalFat, elems[currentFatId].innerHTML);
+        UpdateRemainingElement(elems[remainingProteinId], totalProtein, elems[currentProteinId].innerHTML);
 }
+
+function UpdateRemainingElement(element, total, current)
+{
+    element.innerHTML = parseInt(total)  - parseInt(current.replace(",", ""));
+    SetColorElement(element);
+}
+
+function SetColorElement(element)
+{
+    if(parseInt(element.innerHTML) > 0)
+        element.className = "positive";
+    else
+        element.className = "negative";
+}
+
+function removejscssfile(filename, filetype){
+    var targetelement=(filetype=="js")? "script" : (filetype=="css")? "link" : "none" //determine element type to create nodelist from
+    var targetattr=(filetype=="js")? "src" : (filetype=="css")? "href" : "none" //determine corresponding attribute to test for
+    var allsuspects=document.getElementsByTagName(targetelement)
+ 
+    for (var i=allsuspects.length; i>=0; i--) //search backwards within nodelist for matching elements to remove
+    {
+        if (allsuspects[i] && allsuspects[i].getAttribute(targetattr)!=null && allsuspects[i].getAttribute(targetattr).indexOf(filename)!=-1)
+        allsuspects[i].parentNode.removeChild(allsuspects[i]) //remove element by calling parentNode.removeChild()
+    }
+}
+
 
 function log(str) {
   console.log(str);
@@ -194,7 +229,6 @@ $(function(){
             
             var mealHeaderElement = FindMealHeaderElement();
             
-            
             // cannot end with 'n'
             var postName = "Protei";
             var names = [];
@@ -208,8 +242,20 @@ $(function(){
             ReplaceGoals(goalCalories, goalCarbs, goallFat, goalProtein);
             AddDerivedCalories(mealHeaderElement, indexes);            
             
+            $('.google_ads_with_related_links').remove();
+            removejscssfile("show_ads.js", "js");
             
-        
+            var contentElem = document.getElementsByTagName("content");
+            content.removeChild(content.firstChild);
+            
+            var divs = document.body.children;
+            for (var i=divs.length - 1; i>=0; --i)
+            {
+                if(divs[i].outerHTML.replace('"', '').replace('"', '') == "<div style=border: 1px dashed black;></div>")
+                {
+                    divs[i].parentNode.removeChild(divs[i]);
+                }
+            }
         });
 });
 
