006d0b2114ce17481376f5361a2c753a9fc9d8e0
John-Gee <john.ettedgui@gmail.com>
2015-02-08 02:50:47 -0800

    Fixed the replace function to replace all /dairy/add?
    Signed-off-by: John-Gee <john.ettedgui@gmail.com>

--- MyFitnessPalExtension/content.js
+++ MyFitnessPalExtension/content.js
@@ -22,8 +22,8 @@ function FindElementByClass(classString)
     var elems = document.getElementsByTagName('*'), i;
     
     for(i in elems)
-            if ((" " + elems[i].className + " ").indexOf(" " + classString + " ") > -1)
-                    return elems[parseInt(i)];
+         if ((" " + elems[i].className + " ").indexOf(" " + classString + " ") > -1)
+              return elems[parseInt(i)];
     return undefined;
 }
 
@@ -49,23 +49,23 @@ function AddHeaders(mealHeaderElement, names, afterName, indexes)
     
     for(i in headers)
     {
-        if((headers[i].innerHTML != undefined) && (headers[i].innerHTML.indexOf(afterName) != -1))
-        {
-            afterNode = headers[i];
-            indexes.After = i;
-        }
-        
-        if(headers[i].innerHTML == "Carbs")
-            indexes.Carbs = i;
-        else if(headers[i].innerHTML == "Fat")
-            indexes.Fat = i;
-        else if(headers[i].innerHTML == "Protein")
-            indexes.Protein = i;
+     if((headers[i].innerHTML != undefined) && (headers[i].innerHTML.indexOf(afterName) != -1))
+     {
+         afterNode = headers[i];
+         indexes.After = i;
+     }
+     
+     if(headers[i].innerHTML == "Carbs")
+         indexes.Carbs = i;
+     else if(headers[i].innerHTML == "Fat")
+         indexes.Fat = i;
+     else if(headers[i].innerHTML == "Protein")
+         indexes.Protein = i;
     }
     
     for(i in names)
     {
-        afterNode = InsertClonedNodeAfter(mealHeaderElement, afterNode, names[i]);
+     afterNode = InsertClonedNodeAfter(mealHeaderElement, afterNode, names[i]);
     }
 }
 
@@ -80,7 +80,7 @@ function AddFooters(mealHeaderElement, names, indexes)
     
     for(i in names)
     {
-        afterNode = InsertClonedNodeAfter(tr, afterNode, names[i]);
+     afterNode = InsertClonedNodeAfter(tr, afterNode, names[i]);
     }
 }
 
@@ -91,78 +91,77 @@ function AddDerivedCalories(mealHeaderElement, indexes)
     
     for(i in trs)
     {
-        if(i == 0)
-            continue;
-        
-        var tds = trs[i].getElementsByTagName('td');
-        for(j in tds)
-        {
-            if(j == indexes.After)
-            {
-                var totalCalories = CalculateTotalCaloriesFromStrMacros(tds[indexes.Carbs].innerHTML, tds[indexes.Fat].innerHTML, tds[indexes.Protein].innerHTML);          
-                if(!isNaN(totalCalories))
-                {
-                    var newNode = InsertClonedNodeAfter(trs[i], tds[j], totalCalories);
-                    SetColorElement(newNode);
-                }
-                break;
-            }
-        }
-        
-        if(i == trs.length - 1)
-            break;
+     if(i == 0)
+         continue;
+     
+     var tds = trs[i].getElementsByTagName('td');
+     for(j in tds)
+     {
+         if(j == indexes.After)
+         {
+          var totalCalories = CalculateTotalCaloriesFromStrMacros(tds[indexes.Carbs].innerHTML, tds[indexes.Fat].innerHTML, tds[indexes.Protein].innerHTML);       
+          if(!isNaN(totalCalories))
+          {
+              var newNode = InsertClonedNodeAfter(trs[i], tds[j], totalCalories);
+              SetColorElement(newNode);
+          }
+          break;
+         }
+     }
+     
+     if(i == trs.length - 1)
+         break;
     }
 }
 
 function ReplaceGoals(totalCalories, totalCarbs, totalFat, totalProtein)
 {
-	var classString = "first";
-	var innerString = "Totals";
-	var elems = document.getElementsByTagName('*'), i;
-	
-	var currentLabelId = parseInt(-1);
-	
-	for(i in elems)
-	{
-		if( ((" " + elems[i].className + " ").indexOf(" " + classString + " ") > -1) && (elems[i].innerHTML == innerString) )
-		{
-			currentLabelId = parseInt(i);
-			break;
-		}
-	}
-	
-	var currentCaloriesId = currentLabelId + 1;
-	var currentCarbId = currentCaloriesId + 1;
-	var currentFatId = currentCarbId + 1;
-	var currentProteinId = currentFatId + 1;
-	
-	var goalLabelId = currentProteinId + 4;
-	var caloriesGoalId = goalLabelId + 1;
-	var carbGoalId = caloriesGoalId + 1;
-	var fatGoalId = carbGoalId + 1;
-	var proteinGoalId = fatGoalId + 1;
-	
-	elems[caloriesGoalId].innerHTML = totalCalories;
-	elems[carbGoalId].innerHTML = totalCarbs;
-	elems[fatGoalId].innerHTML = totalFat;
-	elems[proteinGoalId].innerHTML = totalProtein;
-        
-	
-	var remainingLabelId = proteinGoalId + 4;
-	var remainingCaloriesId = remainingLabelId + 1;
-	var remainingCarbId = remainingCaloriesId + 1;
-	var remainingFatId = remainingCarbId + 1;
-	var remainingProteinId = remainingFatId + 1;
-	
-	/*elems[remainingCaloriesId].innerHTML = parseInt(totalCalories)  - parseInt(elems[currentCaloriesId].innerHTML.replace(",", ""));        
-	elems[remainingCarbId].innerHTML = parseInt(totalCarbs) - parseInt(elems[currentCarbId].innerHTML.replace(",", ""));
-	elems[remainingFatId].innerHTML = parseInt(totalFat) - parseInt(elems[currentFatId].innerHTML.replace(",", ""));
-	elems[remainingProteinId].innerHTML = parseInt(totalProtein) - parseInt(elems[currentProteinId].innerHTML.replace(",", ""));*/
-        
-        UpdateRemainingElement(elems[remainingCaloriesId], totalCalories, elems[currentCaloriesId].innerHTML);
-        UpdateRemainingElement(elems[remainingCarbId], totalCarbs, elems[currentCarbId].innerHTML);
-        UpdateRemainingElement(elems[remainingFatId], totalFat, elems[currentFatId].innerHTML);
-        UpdateRemainingElement(elems[remainingProteinId], totalProtein, elems[currentProteinId].innerHTML);
+    var classString = "first";
+    var innerString = "Totals";
+    var elems = document.getElementsByTagName('*'), i;
+    
+    var currentLabelId = parseInt(-1);
+    
+    for(i in elems)
+    {
+        if( ((" " + elems[i].className + " ").indexOf(" " + classString + " ") > -1) && (elems[i].innerHTML == innerString) )
+        {
+            currentLabelId = parseInt(i);
+            break;
+        }
+    }
+    
+    var currentCaloriesId = currentLabelId + 1;
+    var currentCarbId = currentCaloriesId + 1;
+    var currentFatId = currentCarbId + 1;
+    var currentProteinId = currentFatId + 1;
+    
+    var goalLabelId = currentProteinId + 4;
+    var caloriesGoalId = goalLabelId + 1;
+    var carbGoalId = caloriesGoalId + 1;
+    var fatGoalId = carbGoalId + 1;
+    var proteinGoalId = fatGoalId + 1;
+    
+    elems[caloriesGoalId].innerHTML = totalCalories;
+    elems[carbGoalId].innerHTML = totalCarbs;
+    elems[fatGoalId].innerHTML = totalFat;
+    elems[proteinGoalId].innerHTML = totalProtein;
+    
+    var remainingLabelId = proteinGoalId + 4;
+    var remainingCaloriesId = remainingLabelId + 1;
+    var remainingCarbId = remainingCaloriesId + 1;
+    var remainingFatId = remainingCarbId + 1;
+    var remainingProteinId = remainingFatId + 1;
+    
+    /*elems[remainingCaloriesId].innerHTML = parseInt(totalCalories)  - parseInt(elems[currentCaloriesId].innerHTML.replace(",", ""));     
+    elems[remainingCarbId].innerHTML = parseInt(totalCarbs) - parseInt(elems[currentCarbId].innerHTML.replace(",", ""));
+    elems[remainingFatId].innerHTML = parseInt(totalFat) - parseInt(elems[currentFatId].innerHTML.replace(",", ""));
+    elems[remainingProteinId].innerHTML = parseInt(totalProtein) - parseInt(elems[currentProteinId].innerHTML.replace(",", ""));*/
+     
+    UpdateRemainingElement(elems[remainingCaloriesId], totalCalories, elems[currentCaloriesId].innerHTML);
+    UpdateRemainingElement(elems[remainingCarbId], totalCarbs, elems[currentCarbId].innerHTML);
+    UpdateRemainingElement(elems[remainingFatId], totalFat, elems[currentFatId].innerHTML);
+    UpdateRemainingElement(elems[remainingProteinId], totalProtein, elems[currentProteinId].innerHTML);
 }
 
 function UpdateRemainingElement(element, total, current)
@@ -187,10 +186,9 @@ function removejscssfile(filename, filetype){
     for (var i=allsuspects.length; i>=0; i--) //search backwards within nodelist for matching elements to remove
     {
         if (allsuspects[i] && allsuspects[i].getAttribute(targetattr)!=null && allsuspects[i].getAttribute(targetattr).indexOf(filename)!=-1)
-        allsuspects[i].parentNode.removeChild(allsuspects[i]) //remove element by calling parentNode.removeChild()
+            allsuspects[i].parentNode.removeChild(allsuspects[i]) //remove element by calling parentNode.removeChild()
     }
 }
-
 
 function log(str) {
     console.log(str);
@@ -221,31 +219,28 @@ Date.prototype.getFullDate = function() {
     return getFullX(date);
 };
 
-
-
-
 var logDiv = document.createElement("div");
 logDiv.style.border = "1px dashed black";
 document.body.appendChild(document.createElement("br"));
 document.body.appendChild(logDiv);
 
 $(function(){
-        
+     
     chrome.runtime.sendMessage({data: true}, function(response) {
         var json = JSON.parse(response);
         var days = JSON.parse(json[0]);
         var data = JSON.parse(json[1]);
         var fixURL = JSON.parse(json[2]);
-        
+     
         if (fixURL)
         {
             var url = document.URL;
             var dateParam = "?date=";
             if(url.indexOf(dateParam) == -1)
-            {                
+            {          
                 var nextElement = FindElementByClass("next");
                 var nextDate = nextElement.href.substring(nextElement.href.indexOf(dateParam) + 6);
-                
+            
                 var d = new Date(nextDate);
                 d.addDays(-1);
                 var year = d.getFullYear()
@@ -254,14 +249,14 @@ $(function(){
                 
                 var addURL = "/diary/add";
                 var newAddURL = addURL.concat(dateParam, year, "-", month, "-", date);
-                document.body.innerHTML = document.body.innerHTML.replace(addURL, newAddURL);
+                document.body.innerHTML = document.body.innerHTML.replace(/\/diary\/add/g, newAddURL);
             }
         }
-        
+
         var dateElement = FindElementByClass("date");
-        
+
         var day = parseInt(0);
-        
+
         for(var i = 0; i < days.length; ++i)
         {
             if(dateElement.innerHTML.indexOf(days[i]) != -1)
@@ -270,33 +265,33 @@ $(function(){
                 break;
             }
         }
-        
+
         var goalCarbs = data[day].carbs;
         var goallFat = data[day].fat;
         var goalProtein = data[day].protein;
         var goalCalories = data[day].totaldesiredcalories;
-        
+
         var mealHeaderElement = FindMealHeaderElement();
-        
+
         // cannot end with 'n'
         var postName = "Protei";
         var names = [];
         names.push("DerivedCalories");
-        
+
         var indexes = {};
-        
+
         AddHeaders(mealHeaderElement, names, postName, indexes);
         AddFooters(mealHeaderElement, names, indexes);
-        
+
         ReplaceGoals(goalCalories, goalCarbs, goallFat, goalProtein);
-        AddDerivedCalories(mealHeaderElement, indexes);            
-        
+        AddDerivedCalories(mealHeaderElement, indexes);         
+
         $('.google_ads_with_related_links').remove();
         removejscssfile("show_ads.js", "js");
-        
+
         var contentElem = document.getElementsByTagName("content");
         content.removeChild(content.firstChild);
-        
+
         var divs = document.body.children;
         for (var i=divs.length - 1; i>=0; --i)
         {
