cfd9337511e7e8a580395e5145a5f8be9a6dd6a6
John-Gee <john.ettedgui@gmail.com>
2019-06-10 10:45:28 -0700

    Fixed for new url for fixurl

--- MyFitnessPalExtension/content.js
+++ MyFitnessPalExtension/content.js
@@ -287,8 +287,9 @@ $(function(){
                 var month = d.getFullMonth()
                 var date = d.getFullDate()
 
-                var addURL = "/diary/add?";
+                var addURL = "/food/add_to_diary?";
                 var newAddURL = addURL.concat(dateParam, year, "-", month, "-", date, "&");
+                alert(newAddURL);
                 var addFoodElements = FindElementsByClass("add_food");
                 for(var i = 0; i < addFoodElements.length; ++i)
                     addFoodElements[i].href = addFoodElements[i].href.replace(addURL, newAddURL);
